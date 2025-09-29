cc044e3adf252b26e36bd3275b3757809065f3ed
ritikj0536 <124515293+ritikj0536@users.noreply.github.com>
2025-09-29 15:59:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function validEmail(str) {
-  //your JS code here.
+  if (!str) return false; // empty string check
+  
+  // regex for valid email
+  const regex = /^[\w.-]+@[\w.-]+\.[a-zA-Z]{2,}$/;
+  return regex.test(str);
 }
 
 // Do not change the code below.
